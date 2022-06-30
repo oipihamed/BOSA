24c5355d8eb353f6d9e0556be4267b28c0dd201d
Sebas-B <66041490+Sebas-B@users.noreply.github.com>
2022-06-30 01:06:29 -0500

    #Fix: Boton cancelar en RecoveryAccount

--- bosa_app/src/app/pages/auth/recovery/recovery.component.ts
+++ bosa_app/src/app/pages/auth/recovery/recovery.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-recovery',
@@ -7,43 +7,54 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./recovery.component.css']
 })
 export class RecoveryComponent implements OnInit {
-recoveryForm=this.fb.group({
-  email:['',[Validators.required,Validators.email]]
-})
+  recoveryForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email, EmailValidator.cannotContainSpace]]
+  })
 
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
-onLogin(){
+  onLogin() {
     //Se verifica que el formulario sea correcto
-    if(this.recoveryForm.invalid)return;  
+    if (this.recoveryForm.invalid) return;
     //Obtener los datos del formulario
-    const form=this.recoveryForm.value;
+    const form = this.recoveryForm.value;
     console.log(form);
   }
 
-getErrorMessage(field:string){
-    let message='';
-    var form=this.recoveryForm.get(field);
-    if(form!=null){
-     if(form.hasError("required")){
-       message="Campo requerido";
-     }else if(form.hasError("email")){
-       message="Se requiere de un email";
-     }
+  getErrorMessage(field: string) {
+    let message = '';
+    var form = this.recoveryForm.get(field);
+    if (form != null) {
+      if (form.hasError("required")) {
+        message = "Campo requerido";
+      } else if (form.hasError("email")) {
+        message = "Se requiere de un email";
+      } else if (form.hasError("cannotContainSpace")) {
+        message = "El nombre de usuario no puede contener espacios";
+      }
     }
-    return message; 
-   }
+    return message;
+  }
 
-isValidField(field: string){
-    var form=this.recoveryForm.get(field);
-    var flag=false;
-    if(form!=null){
-    flag=form.touched || form.dirty && !form.valid
+  isValidField(field: string) {
+    var form = this.recoveryForm.get(field);
+    var flag = false;
+    if (form != null) {
+      flag = form.touched || form.dirty && !form.valid
     }
     return flag;
+  }
 }
 
+export class EmailValidator {
+  static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
+    if ((control.value as string).indexOf(' ') >= 0) {
+      return { cannotContainSpace: true }
+    }
+
+    return null;
+  }
 }
